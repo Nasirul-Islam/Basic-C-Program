2ca5900e3c5916f49b81594ce49d8c44e05319f4
Nasirul-Islam <nasirulislam866@gmail.com>
2022-05-11 00:20:47 +0600

    user input

--- myfirst.cpp
+++ myfirst.cpp
@@ -4,6 +4,27 @@ using namespace std;
 int main()
 {
     cout<<"Nasirul Islam";
-    cout << "Nasirul" << " Islam" ;
+    cout << "\nNasirul" << " Islam" <<endl ;
+    cout << "Nasirul" <<endl << "Islam" <<endl;
+
+    float num1=1.2340, num2=2.2450;
+    char ch = 'N';
+    char name[14] = "Nasirul Islam";
+
+    cout<< "num1 = " << num1 <<endl <<"num2 = " << num2;
+    cout<< "\n"<< ch <<endl;
+    cout<< name <<endl;
+
+    int n1,n2,sum;
+    char myName[15];
+    cout<< "Enter 2 number: ";
+    cin>> n1 >> n2;
+    cout << n1 <<endl << n2 <<endl;
+    cout <<"sum = " << n1+n2;
+
+    cout << "\nEnter your Name: ";
+    cin>> myName;
+    cout << myName;
+
     return 0;
 }
