ac7237e80de409e3998210b38ecbb089e89ee51d
Nasirul-Islam <nasirulislam866@gmail.com>
2022-05-11 15:18:31 +0600

    area of rectangle

---
+++ areaOfRectangle.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int width, height, area;
+
+    cout << "Enter width and height: ";
+    cin >> width >> height;
+
+    area = width*height;
+
+    cout << "Area = " << area;
+    return 0;
+}
