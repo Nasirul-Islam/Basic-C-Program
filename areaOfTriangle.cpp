8cc5f5c469d787af16f1e551e4f00cec60127a1c
Nasirul-Islam <nasirulislam866@gmail.com>
2022-05-11 15:04:38 +0600

    area of triangle

---
+++ areaOfTriangle.cpp
@@ -0,0 +1,17 @@
+// Area of Triangle
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int base, height, area;
+
+    cout << "Enter Base and Height: ";
+    cin >> base >> height;
+
+    area = .5*base*height;
+
+    cout << "Area = " << area;
+
+    return 0;
+}
