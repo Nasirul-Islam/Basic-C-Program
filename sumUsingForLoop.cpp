0a8df1eee2695170cdb96fddc6108f98189cbcf2
Nasirul-Islam <nasirulislam866@gmail.com>
2022-05-12 12:02:18 +0600

    Sum Using For Loop

---
+++ sumUsingForLoop.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int num, sum=0;
+    cout << "Enter any number: ";
+    cin >> num;
+
+    for(int i=0; i<=num; i++)
+    {
+        sum = sum + i;
+    }
+    cout << "sum = " <<sum;
+}
