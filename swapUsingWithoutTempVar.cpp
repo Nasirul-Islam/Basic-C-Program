073ba3fdc0a49bae46b75cccbe4055be09b8ec99
Nasirul-Islam <nasirulislam866@gmail.com>
2022-05-14 18:00:48 +0600

    swap Using With out Temp Var

--- swapUsingWithoutTempVar.cpp
+++ swapUsingWithoutTempVar.cpp
@@ -1,13 +1,12 @@
-// Swap Using Temp Var
+// Swap Using Without Temp Var
 #include<iostream>
 using namespace std;
 
-int swap(int x, int y)
+int swap(int x, int y) // x=5, y=10
 {
-    int temp;
-    temp = x;
-    x = y;
-    y = temp;
+    x = x+y; // x = 5+10  = 15
+    y = x-y; // y = 15-10 = 5
+    x = x-y; // x = 15-5  = 10
     cout << "X = " << x <<endl << "Y = " << y <<endl;
     return 0;
 }
@@ -17,6 +16,7 @@ int main()
     int x, y;
     cout << "Enter two number: ";
     cin >> x >>y;
+    cout << "X = " << x <<endl << "Y = " << y <<endl <<endl;
     swap(x,y);
     return 0;
 }
